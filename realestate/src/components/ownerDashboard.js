f20b8798d427b11e437dec21eae93dcf98f06696
mangesh fule <mangeshphule00@gmail.com>
2021-03-18 11:42:39 +0530

    Dash

--- realestate/src/components/ownerDashboard.js
+++ realestate/src/components/ownerDashboard.js
@@ -2,6 +2,7 @@ import Axios from "axios";
 import React from "react";
 class OwnerDash extends React.Component {
   state = {
+    id: this.props.user.id,
     landProperties: [
       {
         propertyTitle: "yes",
@@ -21,22 +22,18 @@ class OwnerDash extends React.Component {
   };
   componentDidMount() {
     this.props.onLogin("owner");
+    Axios.get(
+      `http://localhost:8080/realEstate/owner/myProperty/${this.props.user.id}`
+    ).then((res) => {
+      //console.log(res.data);
+      const land = res.data;
+      this.setState({ landProperties: land });
+      console.log(this.state.landProperties, "land");
+      console.log(land)
+    });
   }
 
-  // const {
-  //   propertyTitle,
-  //   propertyArea,
-  //   dimensionLength,
-  //   dimensionBreadth,
-  //   propertyPrice,
-  //   propertyType,
-  //   ownershipType,
-  //   latitude,
-  //   longitude,
-  //   propertyCity,
-  //   propertyPincode,
-  //   propertyRegistDate,
-  // } = res.data;
+  
   // "ownerId": 4,
   // "ownerName": "root",
   // "ownerEmail": "mangesh@gmail.com",
@@ -51,13 +48,55 @@ class OwnerDash extends React.Component {
   render() {
     const { name, email } = this.props.user;
     console.log(this.props.user, "ownerDash");
+    const list = this.state.landProperties;
+    console.log(list , 'list in render')
     return (
       <>
-        <h2>{name}'DashBoard</h2>
-
+        <div className="row">
+          <br />
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <h4>{name}'DashBoard</h4>
+          </div>
+          <div className="col">
+            <h5>
+              <button>+AddNewProperty</button>
+            </h5>
+          </div>
+        </div>
+        <div className="row">
+          <br />
+        </div>
+        <div className="row"></div>
+        {list.map((val) => {
+          return (
+            <div className="cart">
+              <div className="row">
+                <p>
+                  Id : {val.propertyId} Area :{val.propertyArea} 
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </>
     );
   }
 }
 
 export default OwnerDash;
+// const {
+  //   propertyTitle,
+  //   propertyArea,
+  //   dimensionLength,
+  //   dimensionBreadth,
+  //   propertyPrice,
+  //   propertyType,
+  //   ownershipType,
+  //   latitude,
+  //   longitude,
+  //   propertyCity,
+  //   propertyPincode,
+  //   propertyRegistDate,
+  // } = res.data;
