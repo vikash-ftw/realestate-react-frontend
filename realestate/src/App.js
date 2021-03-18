629097d57522f79549fe4f8d4fc3cfdf6ab5a20d
mangesh fule <mangeshphule00@gmail.com>
2021-03-18 10:56:21 +0530

    updateOwnerDone

--- realestate/src/App.js
+++ realestate/src/App.js
@@ -58,6 +58,8 @@ class App extends React.Component {
   logout = () => {
     this.setState({ isLogin: false });
     this.setState({ actorName: "" });
+    localStorage.removeItem("actorType");
+    localStorage.removeItem("actorId");
   };
 
   render() {
@@ -202,7 +204,8 @@ class App extends React.Component {
           <Route
             path="/ownerUpdate"
             render={(props) => (
-              <OwnerUpdate user={this.state.user} {...props} />
+              <OwnerUpdate user={this.state.user}
+              onLogout={this.logout}  {...props} />
             )}
           />
           <Redirect to="/not-found" />
